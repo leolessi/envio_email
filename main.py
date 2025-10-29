2bd80dfe9ec4058ee2d27be63fa9dc866a3ab156
leolessi <leonardolessi@gmail.com>
2025-10-28 23:28:38 -0300

    divisao do codigo em functions / sem teste

--- main.py
+++ main.py
@@ -8,49 +8,72 @@ import smtplib
 # é onde a mensagem do e-mail será construida
 import email.message
 
+# senha ficticia
+senha = 123456
 
 lista_usuarios = [
     {
         "Usuario": "Leonardo",
-        "From": "leonardo@email.com",
-        "To": "destinatario@email.com",
-        "Cc": "emailcopia@email.com",
-        "Bcc": "emailcopiaoculta@email.com",
+        "Destinatario": "Destinatario teste",
+        "From": "leonardo@gmail.com",
+        "To": "destinatario@gmail.com",
+        "Cc": "emailcopia@gmail.com",
+        "Bcc": "emailcopiaoculta@gmail.com",
         "Subject": "Commodo sint laboris ut id. Consequat dolore ex reprehenderit mollit. Commodo cillum labore enim deserunt elit non sunt anim.",
-    }
+    },
+    {
+        "Usuario": "Usuario Teste",
+        "Destinatario": "Destinatario teste",
+        "From": "teste@gmail.com",
+        "To": "destinatario@gmail.com",
+        "Cc": "emailcopia@gmail.com",
+        "Bcc": "emailcopiaoculta@gmail.com",
+        "Subject": "Commodo sint laboris ut id. Consequat dolore ex reprehenderit mollit. Commodo cillum labore enim deserunt elit non sunt anim.",
+    },
 ]
 
 
-def enviar_email():
-    msg = email.message.Message()
-    msg["Subject"] = (
-        "Assunto do e-mail. Duis cillum cillum non cillum ullamco aute cillum irure tempor dolore id adipisicing. Ex fugiat aute nostrud do dolore ipsum commodo. Ut labore qui adipisicing incididunt voluptate voluptate est. Sunt proident aliqua ut ad sint ullamco. Fugiat culpa commodo commodo in minim eu minim ex esse."
-    )
-    msg["From"] = "emaildoremetente@gmail.com"
-    msg["To"] = "emaildodestinatario@gmail.com"
-    msg["Cc"] = "emaildocopia@gmail.com; emaildoremetente+copia@gmail.com"
-    msg["Bcc"] = "emaildocopiaoculta@gmail.com"
+def enviar_email(lista):
+    for usuario in lista_usuarios:
+        msg = email.message.Message()
+        msg["Subject"] = usuario["Subject"]
+        msg["From"] = usuario["From"]
+        msg["To"] = usuario["To"]
+        msg["Cc"] = usuario["Cc"]
+        msg["Bcc"] = usuario["Bcc"]
+        usuario_remetente = usuario["Usuario"]
+        usuario_destinatario = usuario["Destinatario"]
 
-    corpo_email = """
-    <p>Boa tarde, destinatário.</p>
-    <p>Mollit ea incididunt excepteur exercitation incididunt cillum cillum reprehenderit labore commodo. Id do minim aliqua quis mollit incididunt quis consectetur quis nisi ut ex cillum aliqua. Veniam deserunt deserunt mollit esse culpa occaecat dolor velit. Officia cupidatat dolor ex consectetur aliquip deserunt reprehenderit aliquip mollit mollit pariatur. Ullamco est cillum veniam velit excepteur aliquip minim. Non sint qui aute in cupidatat aliquip.</p>
-    <p>Att., Remetente</p>
-    """
+        corpo_email = criar_corpo_email(
+            usuario_destinatario, msg["Subject"], usuario_remetente
+        )
 
+        msg.add_header("Content-Type", "text/html")
+        msg.set_payload(corpo_email)
+        conexao_servidor(msg["From"], senha, msg)
+        print(f"E-mail enviado com sucesso de {msg['From']} para {msg['To']}")
+
+
+def criar_corpo_email(destinatario, assunto, remetente):
+    corpo_email = f"""
+        <p>Boa tarde, {destinatario}.</p>
+        <p>{assunto}</p>
+        <p>Att., {remetente}</p>
+        """
     corpo_email = corpo_email.encode("utf-8")
 
-    msg.add_header("Content-Type", "text/html")
-    msg.set_payload(corpo_email)
+    return corpo_email
 
+
+def conexao_servidor(email_remetente, senha_remetente, mensagem):
     # estabelece conexão com o servidor
     servidor = smtplib.SMTP("smtp.gmail.com", 587)
     # criptografa as informacoes dentro do servidor
     servidor.starttls()
     # ATENÇÃO: Não é recomendado, DE FORMA ALGUMA, deixar a senha exposta. Consulte README.md
-    servidor.login(msg["From"], "senha_envio_emails")
-    servidor.send_message(msg)
+    servidor.login(email_remetente, senha_remetente)
+    servidor.send_message(mensagem)
     servidor.quit()
-    print(f"E-mail enviado com sucesso de {msg['From']} para {msg['To']}")
 
 
-enviar_email()
+enviar_email(lista_usuarios)
